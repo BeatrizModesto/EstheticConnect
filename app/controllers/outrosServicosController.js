5ba5c2647c979efc2b84ab99f7da9f7eef65a0f8
Beatriz Lemos <beatriz.lemos@aluno.ifsp.edu.br>
2026-05-08 11:14:39 -0300

    Add files via upload

---
+++ app/controllers/outrosServicosController.js
@@ -0,0 +1,28 @@
+import OutrosServicosModel from '../models/OutrosServicosModel.js';
+
+export async function OutrosServicos(req, res) {
+
+  try {
+
+    const servicos =
+      await OutrosServicosModel.listarServicos();
+
+    const antesDepois =
+      await OutrosServicosModel.listarAntesDepois();
+
+    res.render('outrosservicos', {
+      servicos,
+      antesDepois
+    });
+
+  } catch (erro) {
+
+    console.log(erro);
+
+    res.status(500).send(
+      'Erro ao carregar página'
+    );
+
+  }
+
+}
---
+++ app/controllers/procedimentosController.js
@@ -0,0 +1,71 @@
+import ProcFacialModel from '../models/ProcFacialModel.js';
+import ProcCorporalModel from '../models/ProcCorporalModel.js';
+import ProcCapilarModel from '../models/ProcCapilarModel.js';
+
+export async function Proc_Faciais(req, res) {
+  try {
+    const procedimentos = await ProcFacialModel.listarProcedimentos();
+    const antesDepois = await ProcFacialModel.listarAntesDepois();
+
+    res.render('proc_faciais', {
+      procedimentos,
+      antesDepois
+    });
+
+  } catch (erro) {
+    console.error(erro);
+    res.send("Erro ao carregar página facial");
+  }
+}
+
+
+export async function Proc_Corporais(req, res) {
+
+  try {
+
+    const procedimentos =
+      await ProcCorporalModel.listarProcedimentos();
+
+    const antesDepois =
+      await ProcCorporalModel.listarAntesDepois();
+
+    res.render('procedimentoscorporais', {
+      procedimentos,
+      antesDepois
+    });
+
+  } catch (erro) {
+
+    console.log(erro);
+
+    res.send('Erro ao carregar página corporal');
+  }
+}
+
+
+export async function Proc_Capilares(req, res) {
+
+  try {
+
+    const procedimentos =
+      await ProcCapilarModel.listarProcedimentos();
+
+    const antesDepois =
+      await ProcCapilarModel.listarAntesDepois();
+
+    res.render('procedimentoscapilares', {
+      procedimentos,
+      antesDepois
+    });
+
+  } catch (erro) {
+
+    console.log(erro);
+
+    res.status(500).send(
+      'Erro ao carregar página capilar'
+    );
+
+  }
+
+}
