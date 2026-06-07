4d8da21d0c6d682fcb4a7df205ec90e5ae8a3046
Beatriz Lemos <beatriz.lemos@aluno.ifsp.edu.br>
2026-06-07 15:25:41 -0300

    Add JSON parsing and cookie parser middleware

--- config/server.js
+++ config/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 const app = express();
 const port = process.env.port || 4000;
 
@@ -7,10 +8,13 @@ app.set('view engine', 'ejs');
 app.set('views', './app/views');
 app.use(express.static('./public'));
 
+
 app.listen(port, () => {
     console.log('Servidor web rodando na porta ', port);
 });
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
 
 export default app;
