fe8d9a6218c9b62d8bf36a313329ced80934127d
BeatrizModesto <beatriz.modestot@gmail.com>
2026-06-11 14:17:05 -0300

    Configura MySQL Railway

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -4,10 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const dbConfig = {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.MYSQLHOST,
+    database: process.env.MYSQLDATABASE,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    port: process.env.MYSQLPORT
 };
 
 const pool = mysql.createPool(dbConfig);
